d2af9ecdeed61504ab7dbd0ced2b4a0f7c7b1eb9
onnik <onni.kivinen@gmail.com>
2025-01-20 22:31:49 +0200

    finishing

--- 1.1_OrientationTasks/05_Task/src/ServicePoint.java
+++ 1.1_OrientationTasks/05_Task/src/ServicePoint.java
@@ -1,59 +1,78 @@
 import java.util.LinkedList;
 import java.util.Scanner;
 
-
 class Customer {
-    private int id;
-    private static int idCount = 1321;
-    private long arrivalTime;
+    private final int id;
+    private static int idCount = 1;
+    private final long arrivalTime;
 
     public Customer() {
         this.id = idCount++;
-        this.arrivalTime = System.currentTimeMillis();}
+        this.arrivalTime = System.currentTimeMillis();
+    }
 
     public int getId() {
-        return id;}
+        return id;
+    }
 
     public long getArrivalTime() {
         return arrivalTime;
     }
 }
 
-
 class CustomerGenerator {
     public static void createCustomer(int amountOfCustomers, ServicePoint servicePoint) {
-        System.out.println("Added: " + amountOfCustomers + " customers.");
+        System.out.println("Added: " + amountOfCustomers + " customers to queue.");
+        
         for (int i = 1; i <= amountOfCustomers; i++) {
             Customer customer = new Customer();
-            servicePoint.addtoQueue(customer);
-            System.out.println("New customer added, ID: " + customer.getId() + "   ("+i+")");
+            servicePoint.addToQueue(customer);
         }
     }
 }
 
-
 public class ServicePoint {
-    private LinkedList<Customer> queue = new LinkedList<>();
+    private final LinkedList<Customer> queue = new LinkedList<>();
     
-    public void addtoQueue(Customer customer) {
+    public void addToQueue(Customer customer) {
         queue.addFirst(customer);
-        System.out.println(customer.getId()+" lisätty queen");}
+    }
 
     Customer removeFromQueue() {
         return queue.removeLast();
     }
 
     public void serve() {
-        Customer customer = removeFromQueue();
+        while (!queue.isEmpty()) {
+            Customer customer = removeFromQueue();
+
+            int serviceTime = (int) ((Math.random() * 3000) + 1000);
+            try {
+                Thread.sleep(serviceTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            long currentTime = System.currentTimeMillis();
+            long responseTime = currentTime - customer.getArrivalTime();
+
+            System.out.println("Customer ID: " + customer.getId() + " served. Time in Queue: " + responseTime / 1000 + "s, Service Time: " + serviceTime / 1000 + "s");
+        }
+
+        System.out.println("All customers served.");
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the amount of customers to serve: ");
-        int amountOfCustomers = scanner.nextInt();
+        int amountOfCustomersToAdd = scanner.nextInt();
 
         ServicePoint servicePoint = new ServicePoint();
-        CustomerGenerator.createCustomer(amountOfCustomers, servicePoint);
+        CustomerGenerator.createCustomer(amountOfCustomersToAdd, servicePoint);
 
+        System.out.println("Total customers in queue: " + servicePoint.queue.size());
+
+        servicePoint.serve();
+        scanner.close();
     }
 }
