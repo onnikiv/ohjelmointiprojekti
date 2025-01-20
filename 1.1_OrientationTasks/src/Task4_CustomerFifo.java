e0412a7bd6d9274cb0fa7e90d2e2effe304df391
onnik <onni.kivinen@gmail.com>
2025-01-20 22:47:26 +0200

    finishing

---
+++ 1.1_OrientationTasks/src/Task4_CustomerFifo.java
@@ -0,0 +1,87 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Task4_CustomerFifo {
+    private int id;
+    private long startTime;
+    private long endTime;
+    private static int idCount = 1;
+
+    public Task4_CustomerFifo() {
+        setId(idCount);
+        idCount++;
+    }
+
+    public int getId() {
+        return id;
+    }
+    private void setId(int id) {
+        if (id > 0) { // varmistus että id on suurempi kuin 0
+            this.id = id;
+        } else {
+            System.out.println("Id:n pitää olla suurempi kuin 0!");
+        }
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(long start) {
+        startTime = start;
+    }
+    public long getEndTime() {
+        return endTime;
+    }
+    public void setEndTime(long end) {
+        endTime = end;
+    }
+    public long calculateTime() {
+        if (endTime >= startTime) {
+            return endTime - startTime;
+        } else {
+            System.out.println("End time must be greater than or equal to start time.");
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            LinkedList<Task4_CustomerFifo> queue = new LinkedList<>();
+            boolean ongoing = true;
+            System.out.println("\nEnter:");
+            System.out.println("0 to Exit");
+            System.out.println("1 to Add customer to queue");
+            System.out.println("2 to Remove the first customer from queue\n"); 
+            
+            while (ongoing) {
+                
+                int command = scanner.nextInt();
+                switch (command) {
+                    case 0 -> {
+                        ongoing = false;
+                        System.out.println("Goodbye!\n");
+                        
+                    }
+                    case 1 -> {
+                        Task4_CustomerFifo customer = new Task4_CustomerFifo();
+                        customer.setStartTime(System.currentTimeMillis());
+                        queue.addFirst(customer);
+                        System.out.println("Customer added - Id: " + customer.getId());
+                        System.out.println(" ".repeat(40) + "(In queue: " + queue.size() + ")");
+                    }
+                    
+                    case 2 -> {
+                        if (!queue.isEmpty()) {
+                            Task4_CustomerFifo customer = queue.removeLast();
+                            customer.setEndTime(System.currentTimeMillis());
+                            System.out.print("Customer removed - ");
+                            System.out.println("Id: " + customer.getId() + " - Time spent: " + customer.calculateTime()/1000 + "s.");
+                            System.out.println(" ".repeat(40) + "(In queue: " + queue.size() + ")");
+                            
+                        } else {System.out.println("No customers to remove!");}
+                    }
+                }
+            } 
+        }
+    }
+}
---
+++ 1.1_OrientationTasks/src/Task5_ServicePoint.java
@@ -0,0 +1,78 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class Customer {
+    private final int id;
+    private static int idCount = 1;
+    private final long arrivalTime;
+
+    public Customer() {
+        this.id = idCount++;
+        this.arrivalTime = System.currentTimeMillis();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public long getArrivalTime() {
+        return arrivalTime;
+    }
+}
+
+class CustomerGenerator {
+    public static void createCustomer(int amountOfCustomers, Task5_ServicePoint servicePoint) {
+        System.out.println("Added: " + amountOfCustomers + " customers to queue.");
+        
+        for (int i = 1; i <= amountOfCustomers; i++) {
+            Customer customer = new Customer();
+            servicePoint.addToQueue(customer);
+        }
+    }
+}
+
+public class Task5_ServicePoint {
+    private final LinkedList<Customer> queue = new LinkedList<>();
+    
+    public void addToQueue(Customer customer) {
+        queue.addFirst(customer);
+    }
+
+    Customer removeFromQueue() {
+        return queue.removeLast();
+    }
+
+    public void serve() {
+        while (!queue.isEmpty()) {
+            Customer customer = removeFromQueue();
+
+            int serviceTime = (int) ((Math.random() * 3000) + 1000);
+            try {
+                Thread.sleep(serviceTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            long currentTime = System.currentTimeMillis();
+            long responseTime = currentTime - customer.getArrivalTime();
+
+            System.out.println("Customer ID: " + customer.getId() + " served. Time in Queue: " + responseTime / 1000 + "s, Service Time: " + serviceTime / 1000 + "s");
+        }
+
+        System.out.println("All customers served.");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the amount of customers to serve: ");
+        int amountOfCustomersToAdd = scanner.nextInt();
+
+        Task5_ServicePoint servicePoint = new Task5_ServicePoint();
+        CustomerGenerator.createCustomer(amountOfCustomersToAdd, servicePoint);
+
+        System.out.println("Total customers in queue: " + servicePoint.queue.size());
+
+        servicePoint.serve();
+        scanner.close();
+    }
+}
