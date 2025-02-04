2ff434ea98212a4354dcd4544fd7cae84a163662
onnik <onni.kivinen@gmail.com>
2025-02-04 14:30:19 +0200

    eduni.distributions

--- eduniStuff/src/Main.java
+++ eduniStuff/src/Main.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import eduni.distributions.*;
 
 public class Main {
+    
+    public static void main(String[] args) {
+
+        Bernoulli bernoulli = new Bernoulli(0.7);
+        
+        // DiscreteGenerator dist = new Poisson(0.5);
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Sample: " + bernoulli.sample());
+            
+        }
+    }
+    
+
 }
+
