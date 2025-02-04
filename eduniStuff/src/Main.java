825e10ee73ee2c1e2c153780a775ebc3b9d6ac90
onnik <onni.kivinen@gmail.com>
2025-02-04 14:37:49 +0200

    plör

--- eduniStuff/src/Main.java
+++ eduniStuff/src/Main.java
@@ -5,10 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         Bernoulli bernoulli = new Bernoulli(0.7);
-        
+
         // DiscreteGenerator dist = new Poisson(0.5);
         
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
+        
             System.out.println("Sample: " + bernoulli.sample());
             
         }
