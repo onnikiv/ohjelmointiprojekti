dbb2726db2cb66e7c63a9c382c7662e77aa94e33
onnik <onni.kivinen@gmail.com>
2025-02-04 14:45:01 +0200

    splörtz

--- eduniStuff/src/Main.java
+++ eduniStuff/src/Main.java
@@ -1,18 +1,27 @@
 import eduni.distributions.*;
+import java.util.ArrayList;
 
 public class Main {
     
     public static void main(String[] args) {
 
-        Bernoulli bernoulli = new Bernoulli(0.7);
+        Bernoulli bernoulli = new Bernoulli(0.01);  // System.nanoTime()
 
         // DiscreteGenerator dist = new Poisson(0.5);
+        ArrayList<Long> lista =  new ArrayList<>();
         
         for (int i = 0; i < 100; i++) {
-        
-            System.out.println("Sample: " + bernoulli.sample());
+            
+            if (bernoulli.sample() == 1) {
+                lista.add(bernoulli.sample());
+                
+            }
+            // System.out.println("Sample: " + bernoulli.sample());
             
         }
+        // pysyvä todennäköisyys, jos lisää just ton seedin, System.nanoTime() nii pitäis 
+        // vaihtuu aina joka kerta mut ei toiminu jostai syyst
+        System.out.println("Todnäk: " + lista.size());
     }
     
 
